99c153841e64152695a4bd7e3de7aff9fbdb5e8e
onio1100 <nonio1100n@gmail.com>
2023-04-05 23:47:17 +0200

    save

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,27 @@ import "../styles/Footer.css";
 export default function Footer() {
     return(
         <footer>
-            
+            <div className="footer__wraper">
+                <div className="footer__left">
+                    <ul className="footer__list">
+                        <li>Contact us</li>
+                        <li>Waranty</li>
+                        <li>Returns</li>
+                        <li>About Us</li>
+                    </ul>
+                </div>
+                <div className="footer__right">
+                    <div className="">
+                        <p></p>
+                        <input />
+                        <span></span>
+                    </div>
+                    <div>
+                        <p><a></a><a></a></p>
+                        <p></p>
+                    </div>
+                </div>
+            </div>    
         </footer>
     )
 }
