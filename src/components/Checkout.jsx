09698f50774946f876da4be588db2f04369eeced
onio1100 <nonio1100n@gmail.com>
2023-04-12 01:21:18 +0200

    s

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -2,14 +2,74 @@ import "../styles/Checkout.css"
 export default function Checkout(props){
     return(
         <div className="checkout">
-            <div className="checkout__left">
-                <h1 className="checkoutl__logo"></h1>
-                <form className="checkoutl__form">
-                    
-                </form>
-            </div>
-            <div className="checkout__right">
 
+            <div className="checkout__left">
+                <div className="checkoutl__content">
+                    <h1 className="checkoutl__logo"></h1>
+                    <form className="checkoutl__form">
+                        
+                        <label>email</label>
+                        <input />
+
+                        <label>newslettercheckbox</label>
+                        <input />
+
+                        <p>Shipping addres</p>
+
+                        <label>country</label>
+                        <select>
+                            <option></option>
+                            <option></option>
+                            <option></option>
+                        </select>
+
+                        <label>name</label>
+                        <input />
+
+                        <label>last name</label>
+                        <input />
+
+                        <label>adress</label>
+                        <input />
+
+                        <label>adress line 2</label>
+                        <input />
+
+                        <label>city</label>
+                        <input />
+
+                        <label>zip code</label>
+                        <input />
+
+                        <label>phone number</label>
+                        <input />
+
+                        <a>return to cart</a>
+
+                        <button></button>
+                    </form>
+                </div>
+            </div>
+            
+            <div className="checkout__right">
+                <div className="checkoutr__content">
+                    <ul>
+
+                    </ul>
+                    <form></form>
+                    <div>
+                        <p>subtotal</p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p>shipping</p>
+                        <p></p>
+                    </div>
+                    <div>
+                        <p>total</p>
+                        <p></p>
+                    </div>
+                </div>
             </div>
         </div>
 
@@ -29,6 +89,7 @@ export default function Checkout(props){
 function ListItem(props){
     return(
         <li>
+
             <p>name: {props.product.title}</p>
             <button onClick={() => props.handleCart(props.product)}>+</button>
             <p>quantity: {props.quantity}</p>
